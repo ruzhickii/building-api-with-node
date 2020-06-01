1fec71718a30a2a0684d4e18fffcc84f41503a8e
ruzhickii <ruzhickiisasha@gmail.com>
2020-06-01 22:36:24 +0300

    created methods: post, put, delete for app

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var artists = [
     {
@@ -16,8 +21,6 @@ var artists = [
         name: "Helloween"
     }
 ];
-
-
 
 
 app.get('/', function(req, res) {
@@ -36,6 +39,30 @@ app.get('/artists/:id', function(req, res) {
     res.send(artist);
 })
 
+app.post('/artists', function(req, res) {
+    var artist = {
+        id: Date.now(),
+        name: req.body.name
+    };
+    artists.push(artist);
+    res.send(artist);
+})
+
+app.put('/artists/:id', function(req, res) {
+    var artist = artists.find(function(artist) {
+        return artist.id === Number(req.params.id)
+    });
+    artist.name = req.body.name;
+    res.sendStatus(200);
+})
+
+app.delete('/artists/:id', function(req, res) {
+    artists = artists.filter(function(artist) {
+        return artist.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
+})
+ 
 app.listen(3012, function() {
     console.log('API app started !');
 })
